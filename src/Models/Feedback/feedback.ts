9d67c3f0742913997b4563bf30f22ef97c29b441
rachel ng <ra8cnjm@berkeley.edu>
2021-03-10 13:53:19 +0800

    Merge branch main into rachel/v1-12

--- src/Models/Feedback/feedback.ts
+++ src/Models/Feedback/feedback.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Experience, { IExperience } from '@Models/Experience/experience';
 import { Document, model, Schema } from 'mongoose';
 import { IUser, User } from '..';
@@ -61,68 +60,3 @@ const FeedbackSchema = new Schema({
 });
 
 export default model<IFeedback>('Feedback', FeedbackSchema);
-=======
-import Experience, { IExperience } from '@Models/ExperienceFolder/experience';
-import { Document, model, Schema } from 'mongoose';
-import { IUser, User } from '..';
-
-
-enum FeedbackType {
-    Private = 'private',
-    Public = 'public'
-  }
-
-export interface IFeedback extends Document {
-    user: IUser;
-    coach: IUser; 
-    experience: IExperience;
-    privacy: FeedbackType;
-    feedbackText: string; 
-    enjoyabilityRating: number;
-    difficultyRating: number;
-    completed: boolean; 
-    personalizedMessage: string
-  }
-
-  const FeedbackSchema = new Schema({
-    user: {
-      type: User,
-      required: true,
-    },
-    coach: {
-      type: User,
-      required: true,
-    },
-    experience: {
-      type: Experience, 
-      required: true
-    },
-    privacy: {
-      type: String,
-      enum: Object.values(FeedbackType),
-      required: true,
-    },
-    feedbackText: {
-      type: String, 
-      required: false
-    },
-    enjoyabilityRating: {
-      type: Number, 
-      required: false
-    },
-    difficultyRating: {
-      type: Number, 
-      required: false
-    },
-    completed: {
-      type: Boolean, 
-      required: true
-    }, 
-    personalizedMessage: {
-      type: String, 
-      required: false
-    }
-  });
-  
-  export default model<IFeedback>('Feedback', FeedbackSchema);
->>>>>>> main
