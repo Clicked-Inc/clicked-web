e47aa2765372447ab512b62ba0527998caf7a0e7
Neil Kamdar <nvk5181@gmail.com>
2021-03-19 12:03:05 -0700

    [V1-43] Tags (#5)
    * [V1-43] finished adding Tags and stories
    
    * [V1-43] changed formatting and Tag declaration
    
    * [V1-43] Need to change the utils
    
    * [neil/V1-43] fixed small errors, ready to merge

--- src/Components/Icon/Assets/index.ts
+++ src/Components/Icon/Assets/index.ts
@@ -2,6 +2,7 @@ import Notification from './Icons/notification.svg';
 import NotificationDot from './Icons/notification_dot.svg';
 import BadgeOne from './Icons/badge_one.svg';
 import CourseLogo from './Icons/course_logo.svg';
+import InkIcon from './Icons/ink_icon.svg';
 import ProjectCardCover from './Icons/project_card_cover.svg';
 import CourseCardCover from './Icons/course_card_cover.svg';
 
@@ -10,6 +11,7 @@ export default {
   NotificationDot,
   BadgeOne,
   CourseLogo,
+  InkIcon
   ProjectCardCover,
   CourseCardCover,
 };
