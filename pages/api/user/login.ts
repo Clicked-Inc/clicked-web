cf50be83d63f3d33d7ed0fa40c27951429fb1556
James Shin <62125337+jamesshin5@users.noreply.github.com>
2021-03-19 11:53:03 -0700

    [V1-51] Backend User Auth (#13)
    * password encrypt, jwt generate in login, auth wrapper, need to test
    
    * password encrypt, jwt generate in login, auth wrapper, need to test
    
    * eupdated package.json and login/register
    
    * Update register.ts
    
    * sign up generates hashed password, login generates jwt, and authenticate wraps routes to only allow authenticated calls
    
    * caelin's comments
    
    * adding to jwt payload for permission checking later
    
    * login should be a get method
    
    * style change comment
    
    * Update register.ts

---
+++ pages/api/user/login.ts
@@ -0,0 +1,65 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { NativeError } from 'mongoose';
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import * as Models from '@Models/index';
+import connect from '@Utils/databaseConnection';
+
+const loginHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  if (req.method !== 'GET') {
+    res.status(421).json({ message: 'Incorrect request type' });
+    return;
+  }
+
+  try {
+    await connect();
+    const { email, password } = req.body;
+    if (email == null || password == null) {
+      res
+        .status(400)
+        .json({ message: 'Login requires both email and password' });
+    }
+
+    // Retrieve user with the given email
+    const user: Models.IUser = await Models.User.findOne(
+      { email },
+      (err: NativeError, user: Models.IUser) => {
+        if (err) {
+          return;
+        }
+        return user;
+      }
+    );
+
+    compare(password, user.password, (err, result) => {
+      if (!err && result) {
+        sign(
+          {
+            uid: user.id,
+            email: user.email,
+            role: user.role,
+          },
+          process.env.JWT_SECRET,
+          { expiresIn: '2 days' },
+          (err, token) => {
+            if (token) {
+              res.status(200).json({ message: 'success', authToken: token });
+            } else {
+              res.status(400).json({ message: err });
+            }
+          }
+        );
+      } else {
+        res.status(401).json({ error: 'Incorrect Password!' });
+      }
+    });
+  } catch (e) {
+    // TODO: more specific error codes based on situation
+    res.status(400).json({ message: 'Registration failed' });
+  }
+};
+
+export default loginHandler;
