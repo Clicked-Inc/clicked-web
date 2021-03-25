25b226d937e49c2c27bea9525cea81d2335ad6ca
neil <nvk5181@gmail.com>
2021-03-25 15:38:56 -0700

    [V1-43] forgot semicolon

--- src/Components/SideNav/SideNav.constants.tsx
+++ src/Components/SideNav/SideNav.constants.tsx
@@ -7,7 +7,7 @@ export const topLabels = (
         ["All Courses", <Icon name="IconCourses"/>, false], 
         ["Feed", <Icon name="IconFeed"/>, false], 
         ["Inspiration", <Icon name="IconInpsiration"/>, false]
-    ])
+    ]);
 export const bottomLabels = (
     [
         ["Guide", <Icon name="IconGuide"/>, false], 
