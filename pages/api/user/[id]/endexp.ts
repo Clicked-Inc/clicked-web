3fc5455addfefdb9f9cfe4e8e44dd40d96435130
rachel ng <ra8cnjm@berkeley.edu>
2021-03-26 14:57:53 +0800

    [V1-16] Added User ability to start and end an experience by adding and updating experienceUser document.

---
+++ pages/api/user/[id]/endexp.ts
@@ -0,0 +1,57 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import * as Models from '@Models/index';
+import connect from '@Utils/databaseConnection';
+import { ObjectId } from 'mongodb';
+// import authGuard from '@Api/authGuard';
+
+const userEndExperienceHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  if (req.method !== 'PUT') {
+    res.status(421).json({ message: 'Incorrect request type' });
+    return;
+  }
+  try {
+    await connect();
+    try {
+      const { id } = req.query;
+      const _id = Array.isArray(id) ? id[0] : id;
+      const { experience } = req.body;
+      const filter = {
+        user: { $in: [new ObjectId(_id)] },
+        experience: { $in: [new ObjectId(experience)] },
+      };
+      console.log(filter);
+      req.body.dateType = 'end';
+      req.body.userType = 'previous';
+      req.body.date = new Date();
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore TS2349
+      const experienceUser = await Models.ExperienceUsers.findOneAndUpdate(
+        filter,
+        req.body,
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+      if (!experienceUser) {
+        console.log('no experence found with this filter');
+        return res.status(400).json({ success: false });
+      }
+      res.status(200).json({ success: true, data: experienceUser });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({
+        success: false,
+        message: 'Ending experience for user failed.',
+      });
+    }
+  } catch (e) {
+    res.status(404).json({ message: 'Unable to connect to the database.' });
+  }
+};
+
+//do i have to use authguard?
+export default userEndExperienceHandler;
---
+++ pages/api/user/[id]/startexp.ts
@@ -0,0 +1,50 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import * as Models from '@Models/index';
+import connect from '@Utils/databaseConnection';
+// import authGuard from '@Api/authGuard';
+
+const userStartExperienceHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  if (req.method !== 'POST') {
+    res.status(421).json({ message: 'Incorrect request type' });
+    return;
+  }
+  try {
+    await connect();
+    try {
+      const { id } = req.query;
+      req.body.user = id;
+      req.body.dateType = 'start';
+      req.body.userType = 'current';
+      req.body.date = new Date();
+      const experienceUsers: Models.IExperienceUsers = new Models.ExperienceUsers(
+        req.body
+      );
+      await experienceUsers
+        .populate('experience')
+        .populate('user')
+        .save()
+        .then(function (data) {
+          res.status(200).send(data);
+          console.log('User has started this added to the database!');
+        })
+        .catch(function (err) {
+          console.log(err);
+          res
+            .status(400)
+            .json({ message: 'User Failed to start this experience' });
+          return;
+        });
+    } catch (e) {
+      console.log('There is an error that we cannot presently determine.');
+      res.status(400).json({ success: false });
+    }
+  } catch (e) {
+    res.status(404).json({ message: 'User not found.' });
+  }
+};
+
+//do i have to use authguard?
+export default userStartExperienceHandler;
