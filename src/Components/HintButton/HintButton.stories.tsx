b46a8c857154c5c08e356318e1de363aab7add80
Henry Chiu <46303723+henryychiu@users.noreply.github.com>
2021-03-31 23:41:32 -0700

    [V1-38][V1-42][V1-50] Hint Button + Milestone Card + Locked Milestone Card (#26)

---
+++ src/Components/HintButton/HintButton.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import HintButton from './HintButton';
+
+export default {
+  title: 'HintButton',
+  component: HintButton,
+};
+
+export const Default = () => <HintButton hint="A very helpful hint" />;
--- src/Components/Tag/Tag.stories.tsx
+++ src/Components/Tag/Tag.stories.tsx
@@ -8,36 +8,36 @@ export default {
 };
 
 const Template1: Story = () => (
-  <Tag variant="orange-white-small" label="Tag Name" icon=""/>
+  <Tag variant="orange-white-small" label="Tag Name" icon="" />
 );
 export const OrangeWhiteSmall = Template1.bind({});
 
 const Template2: Story = () => (
-  <Tag variant="gray-black-small" label="Tag Name" icon=""/>
+  <Tag variant="gray-black-small" label="Tag Name" icon="" />
 );
 export const GrayBlackSmall = Template2.bind({});
 
 const Template3: Story = () => (
-  <Tag variant="orange-white-medium" label="Tag Name" icon=""/>
+  <Tag variant="orange-white-medium" label="Tag Name" icon="" />
 );
 export const OrangeWhiteMedium = Template3.bind({});
 
 const Template4: Story = () => (
-  <Tag variant="white-gray-medium" label="Tag Name" icon=""/>
+  <Tag variant="white-gray-medium" label="Tag Name" icon="" />
 );
 export const WhiteGrayMedium = Template4.bind({});
 
 const Template5: Story = () => (
-  <Tag variant="orange-white-icon" label="Tag Name" icon="InkIcon"/>
+  <Tag variant="orange-white-icon" label="Tag Name" icon="UIUXIcon" />
 );
 export const OrangeWhiteIcon = Template5.bind({});
 
 const Template6: Story = () => (
-  <Tag variant="white-gray-icon1" label="Tag Name" icon="InkIcon"/>
+  <Tag variant="white-gray-icon1" label="Tag Name" icon="UIUXIcon" />
 );
 export const WhiteGrayIconL = Template6.bind({});
 
 const Template7: Story = () => (
-  <Tag variant="white-gray-icon2" label="Tag Name" icon="InkIcon"/>
+  <Tag variant="white-gray-icon2" label="Tag Name" icon="UIUXIcon" />
 );
 export const WhiteGrayIconM = Template7.bind({});
