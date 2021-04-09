cf73780aa5f0fd532bd57ccc028a9d39bb89cae2
neil <nvk5181@gmail.com>
2021-04-09 00:15:17 -0700

    [V1-45.2] Fixed another conflict 3

--- src/Api/authGuard.ts
+++ src/Api/authGuard.ts
@@ -5,11 +5,7 @@ const authGuard = (nextApiHandler: NextApiHandler) => async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
-<<<<<<< HEAD
-  const authToken: string = req.headers.authorization.substring(7);
-=======
   const authToken: string = req.headers.authorization.replace('Bearer ', '');
->>>>>>> 3b2a5f39fa861b12fbf8db72cc3dacdf87e22771
   return verify(authToken, process.env.JWT_SECRET, async (err, decoded) => {
     if (!err && decoded) {
       const obj: any = req.query;
