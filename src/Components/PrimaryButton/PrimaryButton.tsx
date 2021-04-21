45d64168e0ed831ebbb12183385a00dc8721dba8
neil <nvk5181@gmail.com>
2021-04-20 22:23:16 -0700

    [V1-41] fixed the padding

--- src/Components/PrimaryButton/PrimaryButton.tsx
+++ src/Components/PrimaryButton/PrimaryButton.tsx
@@ -13,12 +13,12 @@ type PrimaryButtonProps = {
 const PrimaryButton: React.FC<PrimaryButtonProps> = ({ variant, text }) => {
     if (variant === "large") {
         return (
-            <ChakraButton display="flex" flex-direction="column" fontSize={16} fontWeight="600" color="white" borderRadius="6px" width="224px" height="36px" bg="orange.400">{text}</ChakraButton>
+            <ChakraButton display="flex" flex-direction="column" fontSize={16} fontWeight="600" color="white" borderRadius={6} py={5} px={20} bg="orange.400">{text}</ChakraButton>
         )
     }
     else if (variant === "small") {
         return (
-            <ChakraButton display="flex" flex-direction="column" fontSize={16} fontWeight="600" color="white" borderRadius="6px" py="10px" px="40px" bg="orange.400">{text}</ChakraButton>
+            <ChakraButton display="flex" flex-direction="column" fontSize={16} fontWeight="600" color="white" borderRadius={6} py={5} pl={14} pr={14} bg="orange.400">{text}</ChakraButton>
         )
     }
 }
