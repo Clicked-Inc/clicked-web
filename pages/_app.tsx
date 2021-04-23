2a59174483f125eb2e1ebd51e69d4a7f26154da5
Caelin Sutch <caelinsutch@gmail.com>
2021-04-23 08:56:13 -0700

    Fix PageWrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,10 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from '@Reducers';
+import theme from '../src/Theme';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
