6df65c4264641afebf13a0415bb23cc987d6a59d
Caelin Sutch <caelinsutch@gmail.com>
2021-04-23 08:52:55 -0700

    Integrate Redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { store } from '@Reducers';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <ChakraProvider>
-    <Component {...pageProps} />
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
   </ChakraProvider>
 );
 
