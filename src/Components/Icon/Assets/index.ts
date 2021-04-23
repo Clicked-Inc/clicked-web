15917379bb7f6f8ebe486851f1ea4768e3efd0fb
Kim <kimberley.fan123@berkeley.edu>
2021-04-23 06:42:56 -0700

    [V1-48] done

--- src/Components/Icon/Assets/index.ts
+++ src/Components/Icon/Assets/index.ts
@@ -5,6 +5,8 @@ import CourseLogo from './Icons/course_logo.svg';
 import InkIcon from './Icons/ink_icon.svg';
 import ProjectCardCover from './Icons/project_card_cover.svg';
 import CourseCardCover from './Icons/course_card_cover.svg';
+import ImageIcon from './Icons/image_icon.svg';
+import SmileyIcon from './Icons/smiley_icon.svg';
 
 export default {
   Notification,
@@ -14,4 +16,6 @@ export default {
   InkIcon,
   ProjectCardCover,
   CourseCardCover,
+  ImageIcon,
+  SmileyIcon,
 };
