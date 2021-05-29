62c8b0376d3d7a9dccc2a3d0fd3eec2d3900cb48
kimberleyfan <69925728+kimberleyfan@users.noreply.github.com>
2021-05-29 04:35:17 -0700

    [v1-49] done (#48)

---
+++ src/Components/UnlockedMilestone/UnlockedMilestone.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Icon } from '../Icon';
+import { Box, Center } from '@chakra-ui/react';
+import PrimaryButton from '@Components/PrimaryButton';
+
+const UnlockedMilestone = () => (
+  <Box maxW={689} h={206} shadow="lg" textAlign="center" p={15}>
+    <Center m={4}>
+      <Icon name="MilestoneFlag" />
+    </Center>
+    <Center fontSize="lg">
+      <b>//Team Member// has unlocked Milestone 5: Name of Milestone</b>
+    </Center>
+    <Center color="grey"> November 13, 2020 | 12:06 PM</Center>
+    <Center mt={4}>
+      <PrimaryButton variant="large" text="View Milestone 5"></PrimaryButton>
+    </Center>
+  </Box>
+);
+
+export default UnlockedMilestone;
